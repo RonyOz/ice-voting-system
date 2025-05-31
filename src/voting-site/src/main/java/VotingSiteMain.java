ef79b747f1d7df2db0754fc9bca1fd30cddd6018
JuanJDlp <juanjosedelapava1@gmail.com>
2025-05-31 14:42:26 -0500

    feat: added reliable messaging

--- src/voting-site/src/main/java/VotingSiteMain.java
+++ src/voting-site/src/main/java/VotingSiteMain.java
@@ -13,14 +13,17 @@ public class VotingSiteMain {
 
             ObjectAdapter adapter = com.createObjectAdapter("VotingSiteAdapter");
 
+            RMDestinationPrx dest = RMDestinationPrx.checkedCast(com.propertyToProxy("RMDestination.Proxy"));
+            RMSourcePrx rm = RMSourcePrx.checkedCast(com.propertyToProxy("RMSource.Proxy"));
+            rm.setServerProxy(dest);
+
             //Cuidado con la dependencia circular PLEASE FIX
             VotingSiteController controller = new VotingSiteController();
-            RMDestinationPrx dest = RMDestinationPrx.checkedCast(com.propertyToProxy("RMDestination.Proxy"));
-            RMSourcePrx rm = RMSourcePrx.checkedCast(com.stringToProxy("Sender:tcp -h localhost -p 10010"));
-            rm.setServerProxy(dest);
             VotingSiteImpl votingSiteInterface = new VotingSiteImpl(controller,rm);
             controller.setVotingSiteImpl(votingSiteInterface);
+
             adapter.add(votingSiteInterface, Util.stringToIdentity("VotingSiteInterface"));
+
             // Hasta aqui estoy exponinedo mis interfaces
             adapter.activate();
 
