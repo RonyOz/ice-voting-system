3f9ade8ed712418cecd533b60dcca3e62f51e9e6
Rony Ordoñez <ronyfarido@gmail.com>
2025-05-31 15:08:18 -0500

    refactor: update VotingService initialization and rename endpoint in properties file

--- src/votingService/src/main/java/VotingService.java
+++ src/votingService/src/main/java/VotingService.java
@@ -5,26 +5,18 @@ import com.zeroc.Ice.Util;
 public class VotingService {
     public static void main(String[] args) {
 
-        Communicator com = Util.initialize();
-        VotingServiceImpl imp = new VotingServiceImpl();
-        ObjectAdapter adapter = com.createObjectAdapterWithEndpoints("VotingService", "tcp -h localhost -p 10012");
-        adapter.add(imp, Util.stringToIdentity("VotingService"));
-        System.out.println("Voting Service is running...");
-        adapter.activate();
-        com.waitForShutdown();
+        try(Communicator communicator = Util.initialize(args, "properties.cfg")) {
+            ObjectAdapter adapter = communicator.createObjectAdapter("votingServices");
+            VotingServiceImpl votingService = new VotingServiceImpl();
+            adapter.add(votingService, Util.stringToIdentity("votingService"));
+            adapter.activate();
+            System.out.println("Voting Service is running...");
+            communicator.waitForShutdown();
 
-        // try(Communicator communicator = Util.initialize(args, "properties.cfg")) {
-        //     ObjectAdapter adapter = communicator.createObjectAdapter("VotingService.Endpoints");
-        //     VotingServiceImpl votingService = new VotingServiceImpl();
-        //     adapter.add(votingService, Util.stringToIdentity("VotingService"));
-        //     adapter.activate();
-        //     System.out.println("Voting Service is running...");
-        //     communicator.waitForShutdown();
-
-        // } catch (Exception e) {
-        //     System.err.println("Error initializing communicator: " + e.getMessage());
-        //     e.printStackTrace();
-        //     System.exit(1);
-        // }
+        } catch (Exception e) {
+            System.err.println("Error initializing communicator: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
